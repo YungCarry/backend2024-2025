0dab92f4cb029828f1582dc13efdceb25c963a9f
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 11:05:01 +0200

    kesz 3

---
+++ Feladat3/Feladat3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Feladat3
+{
+    static void Main()
+    {
+
+        Console.Write("Adja meg az 'a' értékét: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Adja meg a 'b' értékét: ");
+        double b = double.Parse(Console.ReadLine());
+
+        if (a == 0)
+        {
+            Console.WriteLine("Nincs megoldása az egyenletnek.");
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine("Az egyenlet megoldása: x = " + x);
+        }
+    }
+}
