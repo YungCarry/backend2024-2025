545ff880f008b28bdbb5205e724aace06bb3a3b9
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 10:58:07 +0200

    kesz

---
+++ Feladat2/Feladat2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Feladat2
+{
+    static void Main()
+    {
+        Console.Write("Adja meg az intervallum elejét: ");
+        int intervallumEleje = int.Parse(Console.ReadLine());
+
+        Console.Write("Adja meg az intervallum végét: ");
+        int intervallumVege = int.Parse(Console.ReadLine());
+
+        if (intervallumEleje > intervallumVege)
+        {
+            Console.WriteLine("Az intervallum hibásan van megadva.");
+        }
+        else
+        {
+            Console.WriteLine("Az intervallum helyesen van megadva.");
+        }
+    }
+}
