629931a10d3d19dc799751d92eb5589027699129
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 12:20:15 +0200

    kesz

---
+++ Signum/Signum/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Adja meg az n-értékét: ");
+double n = double.Parse(Console.ReadLine());
+
+if (n > 0)
+{
+    Console.WriteLine("Poz");
+}
+
+else if ( n < 0)
+{
+    Console.WriteLine("Neg");
+}
+
+else
+{
+    Console.WriteLine("0");
+}
