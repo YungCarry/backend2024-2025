16556ff1a98ed97595e3ecfdc087b5d8cd822df8
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 12:25:34 +0200

    kesz

---
+++ Sorrend/Sorrend/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Add meg az első számot: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.Write("Add meg a második számot: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write("Add meg a harmadik számot: ");
+double c = double.Parse(Console.ReadLine());
+
+
+double[] szamok = { a, b, c };
+Array.Sort(szamok);
+Array.Reverse(szamok);
+
+Console.WriteLine("A számok sorrendben:");
+foreach (double i in szamok)
+{
+    Console.WriteLine(i);
+}
