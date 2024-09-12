4e9702389d0209bfcd9927ab3c4ba7b10aad3448
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 11:06:57 +0200

    kesz 4

---
+++ Feladat4/Feladat4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Feladat4
+{
+    static void Main()
+    {
+
+        Console.Write("Adja meg az első oldal hosszát: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Adja meg a második oldal hosszát: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Adja meg a harmadik oldal hosszát: ");
+        double c = double.Parse(Console.ReadLine());
+
+        if (a + b > c && a + c > b && b + c > a)
+        {
+            Console.WriteLine("A háromszög szerkeszthető.");
+        }
+        else
+        {
+            Console.WriteLine("A háromszög nem szerkeszthető.");
+        }
+    }
+}
