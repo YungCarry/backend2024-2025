0a61d562ce126edd41949a76d7760a5939b55135
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 12:08:45 +0200

    kesz

---
+++ Abs/Abs/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Adje meg az a értékét: ");
+double a = double.Parse(Console.ReadLine()); 
+
+if (a < 0)
+{
+    a = -a;
+    Console.WriteLine(a);
+}
+
+else
+{
+    Console.WriteLine("Hiba");
+}
