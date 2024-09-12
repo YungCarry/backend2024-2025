a13b162cb26cf1d30f9deb4efeada2c408df9615
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 11:22:57 +0200

    7 kesz

---
+++ Feladat7/Feladat7/Program.cs
@@ -0,0 +1,23 @@
+﻿class Feladat7
+{
+    static void Main()
+    {
+        Console.WriteLine("Adja meg a víz hőfokát: ");
+        double hofok = double.Parse(Console.ReadLine());
+
+       if (hofok <= 0)
+        {
+            Console.WriteLine("szilárd");
+        }
+
+       else if (hofok >= 100)
+        {
+            Console.WriteLine("Gőz");
+        }
+
+       else
+        {
+            Console.WriteLine("Folyékony");
+        }
+    }
+}
