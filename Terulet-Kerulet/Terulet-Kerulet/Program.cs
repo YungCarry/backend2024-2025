e5b301895056cb381a20681d989a26cf18a9e7b0
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 12:16:16 +0200

    kesz

---
+++ Terulet-Kerulet/Terulet-Kerulet/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Adja meg a hosszuságot: ");
+double h = double.Parse(Console.ReadLine());
+
+Console.Write("Adja meg a szelesseget: ");
+double sz = double.Parse(Console.ReadLine());
+
+if (h > 0 && sz > 0)
+{
+    Console.WriteLine($"Kerület: {(h + sz) * 2}");
+    Console.WriteLine($"Terület: {h*sz}");
+}
+
+else
+{
+    Console.WriteLine("hibás adat");
+}
