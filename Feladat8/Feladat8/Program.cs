f500e279733949e6b24e157e6287e21149b17f09
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 11:59:16 +0200

    9 kesz

---
+++ Feladat8/Feladat8/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Adja meg az x kordinátát: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Adja meg az y kordinatat: ");
+double y = double.Parse(Console.ReadLine()); 
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1. negyed");
+}
+
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2. negyed");
+}
+
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3. negyed");
+}
+
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4. negyed");
+}
+
+else
+{
+    Console.WriteLine("A pont az origoban van");
+}
---
+++ Feladat9/Feladat9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("ADja meg az érdemjegyet");
+double atlag = double.Parse(Console.ReadLine());
+
+
+string ertekeles = atlag switch
+{
+    >= 1 and <= 1.49 => "elégtelen",
+    >= 1.5 and <= 2.49 => "elégséges",
+    >= 2.5 and <= 3.49 => "közepes",
+    >= 3.5 and <= 4.49 => "jó",
+    >= 4.5 and <= 5 => "kitünő",
+    _ => "Nem valid érték"
+};
+
+Console.WriteLine(ertekeles);
