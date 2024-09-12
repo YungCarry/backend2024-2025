87ed8c95803e16f327e39f25bcfdde4ec45e096a
Yung Carry <c9kerdom@vasvari.org>
2024-09-12 10:49:45 +0200

    kesz

---
+++ Feladat1/Feladat1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Feladat1
+{
+    static void Main()
+    {
+        Console.Write("Kérem, adja meg a foglalt helyek számát: ");
+        int foglaltHelyek = int.Parse(Console.ReadLine());
+
+        Console.Write("Kérem, adja meg az összes helyek számát: ");
+        int osszesHelyek = int.Parse(Console.ReadLine());
+
+        if (foglaltHelyek > osszesHelyek)
+        {
+            Console.WriteLine("Helytelen értékmegadás. A foglalt helyek száma nem lehet több, mint az összes helyek száma!");
+        }
+        else
+        {
+            Console.WriteLine("Az adatok helyesek.");
+        }
+    }
+}
