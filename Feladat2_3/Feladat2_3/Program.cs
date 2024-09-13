8f59b29139816a184697315aa722bb0a130c40ae
Yung Carry <c9kerdom@vasvari.org>
2024-09-13 09:20:56 +0200

    kesz

---
+++ Feladat2_3/Feladat2_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+Console.WriteLine("a");
+for (int i = 1; i <= 9; i++)
+{
+    Console.Write(i);
+    if (i < 9)
+    {
+        Console.Write(",");
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine("\nb");
+for (int i = 1; i <= 19; i += 2)
+{
+    Console.Write(i);
+    if (i < 19)
+    {
+        Console.Write(",");
+    }
+}
+Console.WriteLine();
+
+Console.WriteLine("\nc");
+for (int i = 9; i >= 1; i--)
+{
+    Console.Write(i);
+    if (i > 1)
+    {
+        Console.Write(",");
+    }
+}
+Console.WriteLine();
