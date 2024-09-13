9fd0918b94516cbcfb17585dc84bcf195c860193
Yung Carry <c9kerdom@vasvari.org>
2024-09-13 09:31:45 +0200

    asd

---
+++ Feladat4_3/Feladat4_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+     Console.WriteLine("a) Hárommal osztható számok növekvő sorrendben 20-ig:");
+        for (int i = 0; i <= 20; i += 3)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine("\n");
+
+        Console.WriteLine("b) 15-tel osztható kétjegyű számok csökkenő sorrendben:");
+        for (int i = 99; i >= 15; i -= 15)
+        {
+            if (i >= 10)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+        Console.WriteLine("\n");
+  
